46d38270871b7bcb847951fca306e6e9e7af8d69
xYoikerx <emanuel655144444@gmail.com>
2022-03-18 00:47:46 -0400

    Se guardar al Proyecto

---
+++ botones.js
@@ -0,0 +1,16 @@
+const boton3=document.querySelector('.link-3')
+boton3.addEventListener('click',()=>{
+    window.location.assign("https://github.com/xYoikerx")
+})
+
+
+const boton2=document.querySelector('.link-2')
+boton2.addEventListener('click',()=>{
+    window.location.assign("https://github.com/xYoikerx")
+})
+
+
+const boton1=document.querySelector('.link-1')
+boton1.addEventListener('click',()=>{
+    window.location.assign("https://github.com/xYoikerx")
+})
