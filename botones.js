709957131ad8d7720492518dda1e8d4958be2a82
xYoikerx <emanuel655144444@gmail.com>
2022-03-18 00:52:11 -0400

    Se guardar al Proyecto

--- botones.js
+++ botones.js
@@ -1,16 +1,16 @@
 const boton3=document.querySelector('.link-3')
 boton3.addEventListener('click',()=>{
-    window.location.assign("https://github.com/xYoikerx")
+    window.location.assign("https://github.com/xYoikerx/S.D.E.-System-Developmen-Enterprise-.git")
 })
 
 
 const boton2=document.querySelector('.link-2')
 boton2.addEventListener('click',()=>{
-    window.location.assign("https://github.com/xYoikerx")
+    window.location.assign("https://github.com/xYoikerx/S.D.E.-System-Developmen-Enterprise-.git")
 })
 
 
 const boton1=document.querySelector('.link-1')
 boton1.addEventListener('click',()=>{
-    window.location.assign("https://github.com/xYoikerx")
+    window.location.assign("https://github.com/xYoikerx/S.D.E.-System-Developmen-Enterprise-.git")
 })
